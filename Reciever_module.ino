887194453d053459a898b3f48ef57dacf32aac72
tmktcq <81883339+tmktcq@users.noreply.github.com>
2023-11-07 09:09:34 -0600

    Receiver module code

---
+++ Reciever_module.ino
@@ -0,0 +1,28 @@
+#include <RH_ASK.h>
+#include <SPI.h>
+
+RH_ASK driver; 
+uint8_t buff[32]; 
+uint8_t bufflen = sizeof(buff); 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); 
+  driver.init(); 
+  pinMode(2, OUTPUT); 
+}
+
+String str; 
+void loop() {
+  if(driver.recv(buff,&bufflen))
+  {
+    str = (char*)buff; 
+    if(str == "UNSAFE_LEVELS")
+    {
+      Serial.print("Opening Garage Door\n"); 
+      digitalWrite(2, HIGH); 
+      delay(200); 
+      digitalWrite(2, LOW); 
+    }
+  }
+}
